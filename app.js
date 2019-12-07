22a50805338621045437227c16e5ac7b73c3d7f0
John <jkforsythe@hotmail.com>
2019-12-07 15:33:01 +0000

    ID generator created

--- app.js
+++ app.js
@@ -25,6 +25,26 @@ const ItemCtrl = (function() {
     getItems: function() {
       return data.items;
     },
+    addItem: function(name, calories) {
+      let ID;
+      // Create ID
+      if(data.items.length > 0) {
+        ID = data.items[data.items.length - 1].id + 1;
+      } else {
+        ID = 0;
+      }
+
+      // Calories to number
+      calories = parseInt(calories);
+
+      // Create new item
+      newItem = new Item(ID, name, calories);
+
+      // Add to items array
+      data.items.push(newItem);
+
+      return newItem;
+    },
     logData: function() {
       return data;
     }
@@ -89,7 +109,11 @@ const App = (function(ItemCtrl, UICtrl) {
     // Get form input from UI Controller
     const input = UICtrl.getItemInput();
 
-    console.log(input);
+    // Check for name and calorie input
+    if(input.name !== '' && input.calories !== '') {
+      // Add item 
+      const newItem = ItemCtrl.addItem(input.name, input.calories);
+    }
 
     e.preventDefault();
   }
